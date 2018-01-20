90a3968e6b63646f75955d0e3f2aa5b47722e0d1
ltinyho <ltinyho@gmail.com>
2018-01-20 10:07:52 +0800

    fixed rules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VeeValidate, { Validator } from 'vee-validate';
 import zh_CN from 'vee-validate/dist/locale/zh_CN';
-import rules from './rules';
+import regexs from './rules';
 const dict = {
   name: 'zh_CN',
   custom: {
@@ -31,22 +31,22 @@ const config = {
 const rules = [
   {
     name: 'mobile',
-    regex: rules.mobile,
+    regex: regexs.mobile,
     msg: '请填写正确的手机号码',
   },
   {
     name: 'username',
-    regex: rules.username,
+    regex: regexs.username,
     msg: '请输入正确的用户名，以字母开头，6~16字符，比如：kyy123',
   },
   {
     name: 'nickname',
-    regex: rules.nickname,
+    regex: regexs.nickname,
     msg: '请输入正确的呢称(2-15字符，中英文、数字和下划线)'
   },
   {
     name: 'password',
-    regex: rules.password,
+    regex: regexs.password,
     msg: '密码格式错误(以字母开头，长度在6~18之间，只能包含字母、数字)'
   }
 ];
